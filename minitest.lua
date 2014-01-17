e2bc0e0929e2bf59025d5454415a8f6d5d785224
Cy <whatever>
2014-01-16 19:52:14 -0800

    Removing globals
    Got sick of dealing with them. Contexts work great as unspoken extra arguments to side effect free functions, but lua doesn't  have contexts that I know of, and trying to do it with globals wasn't working. So no more global "describe" or "it" :'(
    
    ...maybe could just setfenv them for each describe to the c: equivalents...

--- minitest.lua
+++ minitest.lua
@@ -1,6 +1,3 @@
-local context
-local root
-
 local ansicolors = require('ansicolors')
 
 local startingTest = ansicolors('%{blue}○')
@@ -10,73 +7,82 @@ local failure = ansicolors('%{red}●')
 
 local backspace = string.char(8)
 
-local errors
-local successes
-local failures
-
-local function minidescribe(name,describer)
-    local save = context
-    context = {
-        name=name,
-        parent = save
-    }
-
-    status, err = pcall(describer)
-    context = save
-    if not status then error(err) end
+local function makeContext(root)
+    function recontext(parent,name)
+        return {
+            root=root,
+            parent=parent,
+            name=name,
+            describe = function (self,name,describer)
+                status, err = pcall(describer,recontext(self,name))
+                if not status then error(err) end
+            end,
+            it = function (self,name,test)
+                io.write(startingTest)
+                status, err = pcall(test)
+                io.write(backspace)
+                maybefuture = err
+                t = type(maybefuture)
+                if status and (t=='function' or (t=="table" and maybefuture.__call)) then
+                    io.write(pending)
+                    oldcontext = context
+                    status, err = pcall(maybefuture)
+                    context = oldcontext
+                end
+                if status then
+                    io.write(success)
+                    root.successes = root.successes + 1
+                else
+                    io.write(failure)
+                    root.failures = root.failures + 1
+                    root.errors[#root.errors+1] = {err=err,name=name,context=self}
+                end
+            end
+        }
+    end
+    return recontext()
 end
 
-local function miniit(name,test)
-    io.write(startingTest)
-    status, err = pcall(test)
-    io.write(backspace)
-    maybefuture = err
-    t = type(maybefuture)
-    if status and (t=='function' or (t=="table" and maybefuture.__call)) then
-        io.write(pending)
-        status, err = pcall(maybefuture)
-        io.write(backspace)
+local function betterError(andThen,...) 
+    olderror = error
+    local function newerror (message)
+        return olderror({message=message,info=debug.getinfo(3),traceback=debug.traceback()})
+    end    
+    _G.error = newerror
+    result = {pcall(andThen, ...)}
+    error = olderror
+    status,err = unpack(result)
+    if status == false then
+        while(type(err) ~= "string") do
+            print(err.traceback)
+            print(err.info.source..":"..err.info.currentline)
+            print('---')
+            err = err.message
+        end
+        error(err)
     end
-    if status then            
-        io.write(success)
-        root.successes = root.successes + 1
-    else
-        io.write(failure)
-        root.failures = root.failures + 1
-        root.errors[#root.errors+1] = {err=err,name=name,context=context}
-    end
+    table.remove(result,1)
+    return unpack(result)
 end
 
 local function withTests(makeTests)
-    oldroot = root
-    olddescribe = describe
-    oldit = it
-    root = {
-        successes = 0,
-        failures = 0,
-        errors = {}
-    }
-    olderror = error
+    context = makeContext({
+            successes = 0,
+            failures = 0,
+            errors = {}
+        })
 
-    _G.describe = minidescribe
-    _G.it = miniit
-    _G.error = function(message)
-        assert(message)
-        return olderror({message=message,info=debug.getinfo(3),traceback=debug.traceback()})
-    end
-    local status,bigerr = pcall(makeTests)
-
-    _G.it = oldit
-    _G.describe = olddescribe
-    _G.error = olderror
+    setfenv(makeTests,getfenv())
+        
+    local status,bigerr = pcall(makeTests,context)
 
     io.write('\n')
 
-    for i,err in ipairs(root.errors) do
+    for i,err in ipairs(context.root.errors) do
         io.write('Test ')
         local ctx = err.context
-        local first = true    
-        while(ctx) do
+        local first = true
+        while ctx and ctx.name do
             if first then
                 first = false
             else
@@ -91,40 +97,31 @@ local function withTests(makeTests)
             io.write('/')
         end
         io.write(err.name..' failed!')
-        print(err.err.message)
-        print(err.err.info.source..":"..err.err.info.currentline)
+        err = err.err
+        print(err.message)
+        print(err.info.source..":"..err.info.currentline)
         --print(err.err.traceback)
     end
-    if not status then 
-        local toperr = bigerr
-        while toperr and type(toperr) ~= 'string' do
-            print(toperr.traceback)
-            toperr = toperr.message
-        end
-        if not toperr then
-            error("nil argh")
-        else
-            error(toperr) 
-        end
-    end
+    if not status then error(bigerr) end
     
     -- I am so finicky
     local succname
     local failname
-    if root.successes ~= 1 then
+    if context.root.successes ~= 1 then
         succname = 'successes'
     else
         succname = 'success'
     end
-    if root.failures ~= 1 then
+    if context.root.failures ~= 1 then
         failname = 'failures'
     else
         failname = 'failure'
     end
 
-    print(ansicolors(string.format('%%{green}%d '..succname..' %%{red}%d '..failname,root.successes,root.failures)))
+    print(ansicolors(string.format('%%{green}%d '..succname..' %%{red}%d '..failname,
+        context.root.successes,
+        context.root.failures)))
 
-    root = oldroot
 end
 
-return withTests
+return function(makeTests) betterError(withTests,makeTests) end
